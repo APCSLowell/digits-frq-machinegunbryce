a2abbd739ab72b84b308d535bda9af7eee02a8a7
machinegunbryce <brchan1@s.sfusd.edu>
2025-04-11 07:03:35 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -7,12 +7,25 @@ public class Digits
 
 	public Digits(int num)
 	{ /* to be implemented in part (a) */ 
-	    
+	    Digits = new ArrayList  <Integer>();
+		if(num == 0){
+			Digits.add(0);
+		}
+		int n = num;
+		while (num >0){
+		Digits.add(0, n%10);
+		n=n/10;
+		}
 	}
 
 	public boolean isStrictlyIncreasing()
 	{ /* to be implemented in part (b) */
-		
+		for(int i = 0; i < digitList.size()+1; i ++){
+		if(digitlist.get(i) > digitlist.get(i+1){
+			return false;
+		}
+		}
+		return true;
 
 	}
 	
